07f3658672d1a4e065ebd6f6374856506d509b29
Driton-Gashi <69894595+Driton-Gashi@users.noreply.github.com>
2023-01-02 17:39:24 +0100

    Fixed a bug on Collection.js file

--- js/collection.js
+++ js/collection.js
@@ -4,35 +4,36 @@ let mobs = [
     name: "Zombie",
     likeNr: Math.floor(Math.random() * 50),
     viewNr: Math.floor(Math.random() * 150),
-  },  {
+  },
+  {
     src: "../assets/img/mobs/evoker.png",
     name: "Warden",
     likeNr: Math.floor(Math.random() * 50),
     viewNr: Math.floor(Math.random() * 150),
-  }
-
+  },
 ];
 
 let content = document.querySelector(".collection-body");
-let newBox = document.createElement("div");
-newBox.classList.add("collection-box");
 mobs.forEach((mob) => {
-  newBox.innerHTML = `
-            <div class="collection-box-body">
-              <img src="${mob.src}" alt="" />
-              <h1 class="collection-box-title">${mob.name}</h1>
-            </div>
-            <div class="collection-box-footer">
-              <span class="view-wrapper"
-                ><i class="bi bi-eye-fill"></i>
-                <span class="view-number">&ThinSpace; ${mob.viewNr}</span>
-              </span>
+  let newBox = document.createElement("div");
+  newBox.classList.add("collection-box");
 
-              <span class="like-wrapper"
-                ><i class="bi bi-suit-heart"></i
-                ><span class="like-number">&ThinSpace; ${mob.likeNr}</span>
-              </span>
-            </div>
+  newBox.innerHTML = `
+  <div class="collection-box-body">
+  <img src="${mob.src}" alt="" />
+  <h1 class="collection-box-title">${mob.name}</h1>
+</div>
+<div class="collection-box-footer">
+  <span class="view-wrapper"
+    ><i class="bi bi-eye-fill"></i>
+    <span class="view-number">&ThinSpace; ${mob.viewNr}</span>
+  </span>
+
+  <span class="like-wrapper"
+    ><i class="bi bi-suit-heart"></i
+    ><span class="like-number">&ThinSpace; ${mob.likeNr}</span>
+  </span>
+</div>
             `;
   content.appendChild(newBox);
 });
